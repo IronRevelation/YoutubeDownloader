7e6cf4c19def356aa1e33346d0f9169f9e2c8d03
IronRevelation <69801595+IronRevelation@users.noreply.github.com>
2021-01-04 20:14:22 +0100

    refactoring code pt1

--- main.py
+++ main.py
@@ -1,60 +1,80 @@
 import pafy
 import PySimpleGUI as sg
 import os
-
+import threading
 
 def to_raw(string):
     return fr"{string}"
 
 
+def remove_bad_characters(string):
+    string = to_raw(video.title)
+    string = string.replace('"', '')
+    string = string.replace('/', '-')
+    string = string.replace('\\', '-')
+    string = string.replace('>', '')
+    string = string.replace('<', '')
+    string = string.replace('|', '-')
+    string = string.replace('?', '')
+    string = string.replace('*', '')
+    string = string.replace(':', ' ')
+    return string
+
+
+def convert_format(file_name, previous_extension, extension):
+    os.system('ffmpeg.exe -y -i "' + file_name + '.' + previous_extension + '" "' + file_name + '.' + extension + '"')
+
+
+def download_video(video):
+    title = remove_bad_characters(video.title)
+    best_video = video.getbest()
+    extension = best_video.extension
+    best_video.download(title + '.' + extension)
+    if best_video.extension != 'mp4':
+        convert_format(title, extension, "mp4")
+        os.remove(title + '.' + extension)
+
+
+def download_audio(video):
+    title = remove_bad_characters(video.title)
+    audio = video.getbest()
+    extension = audio.extension
+    audio.download(title + '.' + audio.extension)
+    if audio.extension != 'mp3':
+        convert_format(title, extension, "mp3")
+        os.remove(title + '.' + extension)
+
+
 layout = [[sg.Text('Enter video link:'), sg.InputText()],
-            [sg.Text('Select type:'), sg.Combo(['Video', 'Audio'], default_value='Video', readonly=True)],
-            [sg.Text(size=(40,1), key='-OUTPUT-')],
-            [sg.Button('Download'), sg.Button('Quit')]]
-
-# Create the Window
+              [sg.Text('Select type:'), sg.Combo(['Video', 'Audio'], default_value='Video', readonly=True)],
+              [sg.Text(size=(40, 1), key='-OUTPUT-')],
+              [sg.Button('Download'), sg.Button('Quit')]]
 window = sg.Window('Youtube downloader', layout, icon="logo.ico")
-# Event Loop to process "events" and get the "values" of the inputs
-while True:
-    event, values = window.read()
-    if event == sg.WIN_CLOSED or event == 'Quit':
-        break
-    try:
 
-        video = pafy.new(values[0])
-        raw_title = to_raw(video.title)
-        raw_title = raw_title.replace('"', '')
-        raw_title = raw_title.replace('/', '-')
-        raw_title = raw_title.replace('\\', '-')
-        raw_title = raw_title.replace('>', '')
-        raw_title = raw_title.replace('<', '')
-        raw_title = raw_title.replace('|', '-')
-        raw_title = raw_title.replace('?', '')
-        raw_title = raw_title.replace('*', '')
-        raw_title = raw_title.replace(':', ' ')
 
-        if values[1] == 'Video':
-            best_video = video.getbest()
-            best_video.download(raw_title + '.' + best_video.extension)
-            if best_video.extension != 'mp4':
-                os.system('ffmpeg.exe -y -i "'+raw_title + '.' + best_video.extension+'" "' + raw_title +'.mp4"')
-                os.remove(video.title + '.' + best_video.extension)
+if __name__ == '__main__':
+    while True:
+        event, values = window.read()
+        if event == sg.WIN_CLOSED or event == 'Quit':
+            break
+        if event == 'Download':
+            window['-OUTPUT-'].update("Downloading...")
+            try:
+                video = pafy.new(values[0])
+                if values[1] == 'Video':
+                    t1 = threading.Thread(target=download_video, args=(video,), daemon=True)
+                    t1.start()
+                elif values[1] == 'Audio':
+                    t1 = threading.Thread(target=download_audio, args=(video,), daemon=True)
+                    t1.start()
+                else:
+                    window['-OUTPUT-'].update("Unknown error, please retry")
+                    continue
+                window['-OUTPUT-'].update("File successfully downloaded!")
+            except Exception as e:
+                print(e)
+                window['-OUTPUT-'].update("Error: invalid link, please retry")
 
-        elif values[1] == 'Audio':
-            audio = video.getbestaudio()
-            audio.download(raw_title + '.' + audio.extension)
-            if audio.extension != 'mp3':
-                os.system('ffmpeg.exe -y -i "'+raw_title + '.' + audio.extension+'" "' + raw_title +'.mp3"')
-                os.remove(raw_title + '.' + audio.extension)
-
-        else:
-            window['-OUTPUT-'].update("Unknown error, please retry")
-            continue
-
-        window['-OUTPUT-'].update("File successfully downloaded!")
-    except:
-        window['-OUTPUT-'].update("Error: invalid link, please retry")
-
-window.close()
+    window.close()
 
 
